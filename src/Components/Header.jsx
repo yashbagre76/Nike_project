8c287a1f14851b84d154d54ae5779887a06e09f1
Yash Bagre <yashbagre603@gmail.com>
2024-12-25 21:54:36 +0530

    add Header component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,10 +2,9 @@ import React from "react";
 
 function Header() {
   return (
-    <>
-      <div></div>
-    </>
+    <div>
+      <h1> helllo js</h1>
+    </div>
   );
 }
-
 export default Header;
