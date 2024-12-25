9c8a0f979db3f0bf20e7e1909ba4063fcc16c1b4
Yash Bagre <yashbagre603@gmail.com>
2024-12-25 22:01:32 +0530

    add Tailwind Config.js

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function Header() {
   return (
     <div>
-      <h1> helllo js</h1>
+      <h1 className="  bottom-1 placeholder-gray-200 text-wrap bg-orange-900">
+        hello js
+      </h1>
     </div>
   );
 }
