26e562b89418d57c5bc31983b8c36ae85e616339
Yash Bagre <yashbagre603@gmail.com>
2024-12-26 17:22:18 +0530

    add new-components

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,6 +2,22 @@ import React from "react";
 
 const Hero = () => {
   return;
+  <main>
+    <div className="">
+      <h1> YOUR FEET DESERVE THE BEST </h1>
+
+      <p>
+        YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOES .
+        YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOES .{" "}
+      </p>
+    </div>
+    <div>
+      <button className=""> Shop Now</button>
+      <button className=""> Category </button>
+
+      <div className=""></div>
+    </div>
+  </main>;
 };
 
 export default Hero;
