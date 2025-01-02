b662a60b9c30ba602c5ec458f3eb1faba6488ed5
Yash Bagre <yashbagre603@gmail.com>
2025-01-02 17:31:16 +0530

    add imges ro assests

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function Products() {
   const shoesInfo = [
     {
-      img: "https://www.nike.com/in/t/air-jordan-1-retro-high-og-first-in-flight-shoes-fTHb42/FD2596-400",
+      id: 1,
+      name: "Air Jordern 1 Low Primum",
+      img: "",
     },
   ];
 
